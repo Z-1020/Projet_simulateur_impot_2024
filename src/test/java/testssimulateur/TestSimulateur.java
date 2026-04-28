7de02f336b374b99764c775111a3304b2c1b8117
Nathanael Heyberger <nathanael.heyberger@etu.unicaen.fr>
2026-04-28 22:13:07 +0200

    Feat(Test) : EXG_IMPOT_04 implementation

--- src/test/java/testssimulateur/TestSimulateur.java
+++ src/test/java/testssimulateur/TestSimulateur.java
@@ -78,4 +78,23 @@ public class TestSimulateur {
             assertEquals(Double.valueOf(nbPartsAttendues), calculateur.getNbPartsFoyerFiscal());
         }
     }
+
+    @ParameterizedTest(name = "EXG_IMPOT_04 {5}")
+    @CsvFileSource(resources = "/EXG_IMPOT_04_TestData.csv", numLinesToSkip = 1)
+    public void testEXG_IMPOT_04(int revenuNet, String situationMaritale, int nbEnfants, int nbEnfantsH, boolean parentIsole, double nbParts, int revenuFiscalDeReference, double revenuFiscalDeReferenceParParts, int impotAvantDecote, String description) {
+
+        // Arrange
+        calculateur.setRevenusNet(revenuNet);
+        calculateur.setSituationFamiliale(SituationFamiliale.valueOf(situationMaritale));
+        calculateur.setNbEnfantsACharge(nbEnfants);
+        calculateur.setNbEnfantsSituationHandicap(nbEnfantsH);
+        calculateur.setParentIsole(parentIsole);
+
+
+        // Act
+        calculateur.calculImpotSurRevenuNet();
+
+        // Assert
+        assertEquals(impotAvantDecote, calculateur.getImpotAvantDecote());
+    }
 }
